99ea97a270e705e4c8cbad66fb260da0d2703292
Nick Rothwell <nick@cassiel.com>
2014-07-26 12:42:52 +0100

    Midje.

--- test/eu/cassiel/nanomator_test.clj
+++ test/eu/cassiel/nanomator_test.clj
@@ -1,7 +1,6 @@
 (ns eu.cassiel.nanomator-test
-  (:require [clojure.test :refer :all]
-            [eu.cassiel.nanomator :refer :all]))
+  (:require [eu.cassiel.nanomator :refer :all]
+            [midje.sweet :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact "foo"
+      "X" => "X")
