00717a2f4f075051a31a5e465f43d93cc445249c
Nick Rothwell <nick@cassiel.com>
2014-07-26 19:16:15 +0100

    First Codox setup.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to nanomator
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -6,4 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles
   {:dev {:dependencies [[midje "1.6.3"]]
-         :plugins [[lein-midje "3.1.3"]]}})
+         :plugins [[lein-midje "3.1.3"]
+                   [codox "0.8.10"]]
+         :codox {:output-dir "../../cassiel.gh-pages/nanomator"
+                 :defaults {:doc/format :markdown}
+                 :src-dir-uri "http://github.com/cassiel/nanomator/blob/master/"
+                 :src-linenum-anchor-prefix "L"}}})
