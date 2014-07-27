93470213a42912e1ad87f5ae751ab747c311b48c
nick rothwell <nick@cassiel.com>
2014-07-27 14:53:32 +0100

    First code, README.

--- src/eu/cassiel/nanomator.clj
+++ src/eu/cassiel/nanomator.clj
@@ -1,6 +1,38 @@
-(ns eu.cassiel.nanomator)
+(ns eu.cassiel.nanomator
+  "Nano-automation system.")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn initial
+  "Initial system state. Takes an optional map of starting values for channels
+   which, at the earliest timestamp, we want to be other than zero."
+  [& {:keys [init] :or {init {}}}]
+  {:current init
+   :fade-sequences {}})
+
+(defn automate
+  "Add an automation segment to a channel `ch`. The segment starts at time, `ts`,
+   lasts for `dur` frames and fades from the current value to `target`. The
+   target value will actually be hit at time `(+ ts dur 1)` (so `dur` is treated
+   as a time measure, not a frame count).
+
+   Returns a new state, with this segment added, even if we're notionally beyond
+   the end of the segment: the state doesn't track the last-executed time.
+
+   Don't overlap segments. Bad things will happen. (Actually, segments will be applied
+   in increasing order of starting stamp.)"
+    [state ch ts dur target]
+  )
+
+(defn sample
+  "Sample a channel `ch` at a time `ts`. If there are expired segments, the last one
+   will be applied to determine the current channel value. Then, any in-scope segment
+   will be interpolated.
+
+   We return the interpolated value, but make no change to the state; it might make
+   sense to [[purge]] the state from time to time."
+  [{c :current} ch ts]
+  (or (get c ch) 0))
+
+(defn purge
+  "Purge this state, catching up to time `ts`. "
+  [state ts]
+  )
--- test/eu/cassiel/nanomator_test.clj
+++ test/eu/cassiel/nanomator_test.clj
@@ -1,6 +1,33 @@
 (ns eu.cassiel.nanomator-test
-  (:require [eu.cassiel.nanomator :refer :all]
+  (:require [eu.cassiel.nanomator :as nn]
             [midje.sweet :refer :all]))
 
-(fact "foo"
-      "X" => "X")
+(fact "Unused channels return 0"
+      (nn/sample (nn/initial) 0 :WHATEVER) => 0)
+
+(fact "Can sample an initial channel."
+      (nn/sample (nn/initial :init {:X 42}) :X 0) => 42)
+
+(fact "Start of fade"
+      (as-> (nn/initial :init {:X 99}) S
+            (nn/automate S :X 100 20 0)
+            (nn/sample S :X 100))
+      => (roughly 99))
+
+(fact "Simple fade"
+      (as-> (nn/initial) S
+            (nn/automate S :X 100 2 1.0)
+            (nn/sample S :X 101))
+      => (roughly 0.5))
+
+(fact "End of fade"
+      (as-> (nn/initial :init {:X 99}) S
+            (nn/automate S :X 100 20 55)
+            (nn/sample S :X 120))
+      => (roughly 55))
+
+(fact "Past end of fade"
+      (as-> (nn/initial :init {:X 99}) S
+            (nn/automate S :X 100 20 55)
+            (nn/sample S :X 999))
+      => (roughly 55))
