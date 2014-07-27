68921d10e1e4cebb5c11ea06cb5e20fb8944cc53
nick rothwell <nick@cassiel.com>
2014-07-27 20:14:13 +0100

    More implementation.

--- src/eu/cassiel/nanomator.clj
+++ src/eu/cassiel/nanomator.clj
@@ -4,9 +4,13 @@
 (defn initial
   "Initial system state. Takes an optional map of starting values for channels
    which, at the earliest timestamp, we want to be other than zero."
-  [& {:keys [init] :or {init {}}}]
-  {:current init
-   :fade-sequences {}})
+  [& {:keys [init interp]}]
+  {:interp interp
+   ;; Sequences: map from chan-name to {:fades, :current}.
+   :sequences (reduce-kv (fn [m k v] (assoc m k {:fades nil :current v}))
+                         nil
+                         init)
+   :time 0})
 
 (defn automate
   "Add an automation segment to a channel `ch`. The segment starts at time, `ts`,
@@ -19,20 +23,72 @@
 
    Don't overlap segments. Bad things will happen. (Actually, segments will be applied
    in increasing order of starting stamp.)"
-    [state ch ts dur target]
-  )
+  [state ch start-ts dur target]
+  (update-in state
+             [:sequences ch]
+             (fn [seqs]
+               (update-in seqs [:fades]
+                          #(as-> % L
+                                 (conj L {:start start-ts :dur dur :target target})
+                                 (sort-by :start L))))))
+
+(defn apply-to-fades
+  "Apply function to fade lists of all channels"
+  [state f]
+  (update-in state
+             [:sequences]
+             (partial reduce-kv (fn [m k v] (assoc m k (update-in v [:fades] f))) nil)))
+
+(defn apply-to-sequences
+  "Apply function to sequences (fades * current) of all channels"
+  [state f]
+  (update-in state
+             [:sequences]
+             (partial reduce-kv (fn [m k v] (assoc m k (f v))) nil)))
+
+(defn interp-default
+  "Default interpolation"
+  [start end pos]
+  (+ start (* (- end start) pos)))
+
+(defn apply-fade
+  "Apply a fade to a current value, return new current value (unchanged if fade in the future)."
+  [{:keys [start dur target]} ts current]
+  (cond (<= ts start)
+        current
+
+        (>= ts (+ start dur))
+        target
+
+        ;; duration 0? TESTME
+        :else
+        (interp-default current target (/ (- ts start) dur))))
+
+(defn purge-to
+  "Purge a sequence to a timestamp."
+  [{:keys [fades current] :as sequence} ts]
+  (cond (nil? fades)
+        sequence
+
+        (> (:start (first fades)) ts)
+        sequence
+
+        :else
+        (let [[hd tl] fades
+              new-current (apply-fade hd ts current)]
+          (recur {:fades tl
+                  :current new-current} ts))))
+
+(defn locate
+  "Change the location of this state to timestamp `ts`, returning a new state. Expired
+   fades will be applied at their last points and purged (so winding time back again
+   will not restore them)."
+  [state ts]
+  (-> state
+      (assoc :time ts)
+      (apply-to-sequences #(purge-to % ts))))
 
 (defn sample
-  "Sample a channel `ch` at a time `ts`. If there are expired segments, the last one
-   will be applied to determine the current channel value. Then, any in-scope segment
-   will be interpolated.
-
-   We return the interpolated value, but make no change to the state; it might make
-   sense to [[purge]] the state from time to time."
-  [{c :current} ch ts]
-  (or (get c ch) 0))
-
-(defn purge
-  "Purge this state, catching up to time `ts`. "
-  [state ts]
-  )
+  "Sample a channel `ch` at the state's current time."
+  [{:keys [current time]} ch]
+  (or (get current ch) 0))
--- test/eu/cassiel/nanomator_test.clj
+++ test/eu/cassiel/nanomator_test.clj
@@ -3,31 +3,103 @@
             [midje.sweet :refer :all]))
 
 (fact "Unused channels return 0"
-      (nn/sample (nn/initial) 0 :WHATEVER) => 0)
+      (nn/sample (nn/initial) :WHATEVER) => 0)
 
 (fact "Can sample an initial channel."
-      (nn/sample (nn/initial :init {:X 42}) :X 0) => 42)
+      (nn/sample (nn/initial :init {:X 42}) :X) => 42)
 
 (fact "Start of fade"
-      (as-> (nn/initial :init {:X 99}) S
-            (nn/automate S :X 100 20 0)
-            (nn/sample S :X 100))
+      (-> (nn/initial :init {:X 99})
+          (nn/automate :X 100 20 0)
+          (nn/locate 100)
+          (nn/sample :X))
       => (roughly 99))
 
 (fact "Simple fade"
-      (as-> (nn/initial) S
-            (nn/automate S :X 100 2 1.0)
-            (nn/sample S :X 101))
+      (-> (nn/initial)
+          (nn/automate :X 100 2 1.0)
+          (nn/locate 101)
+          (nn/sample :X))
       => (roughly 0.5))
 
 (fact "End of fade"
-      (as-> (nn/initial :init {:X 99}) S
-            (nn/automate S :X 100 20 55)
-            (nn/sample S :X 120))
+      (-> (nn/initial :init {:X 99})
+          (nn/automate :X 100 20 55)
+          (nn/locate 120)
+          (nn/sample :X))
       => (roughly 55))
 
 (fact "Past end of fade"
-      (as-> (nn/initial :init {:X 99}) S
-            (nn/automate S :X 100 20 55)
-            (nn/sample S :X 999))
+      (-> (nn/initial :init {:X 99})
+          (nn/automate :X 100 20 55)
+          (nn/locate 999)
+          (nn/sample :X))
       => (roughly 55))
+
+(fact "safe purge"
+      (-> (nn/initial)
+          (nn/automate :my-param 100 10 9.9)
+          (nn/locate 50)
+          (nn/sample :my-param))
+      => (roughly 0))
+
+(fact "partial purge"
+      (-> (nn/initial)
+          (nn/automate :my-param 100 10 10.0)
+          (nn/locate 105)
+          (nn/sample :my-param))
+      => (roughly 5.0))
+
+(fact "locate purges (1)"
+      (-> (nn/initial)
+          (nn/automate :my-param 100 10 9.9)
+          (nn/locate 150)
+          (nn/sample :my-param))
+      => (roughly 9.9))
+
+(fact "locate purges (2)"
+      (-> (nn/initial)
+          (nn/automate :my-param 100 10 9.9)
+          (nn/locate 150)
+          (nn/locate 50)
+          (nn/sample :my-param))
+      => (roughly 9.9))
+
+(fact "pre-insert fade"
+      (-> (nn/initial)
+          (nn/locate 100)
+          (nn/automate :my-param 50 10 10.0)
+          (nn/sample :my-param))
+      => (roughly 10.0))
+
+(fact "pre-insert fade 2"
+      (-> (nn/initial)
+          (nn/locate 105)
+          (nn/automate :my-param 100 10 10.0)
+          (nn/sample :my-param))
+      => (roughly 5.0))
+
+(fact "can handle fades with zero duration"
+      (-> (nn/initial)
+          (nn/automation :my-param 50 0 1.0)
+          (nn/locate 100)
+          (nn/sample :my-param)))
+
+(future-fact "Simple interpolation (use interpolation position as value"
+             (-> (nn/initial :interp {:A (fn [start end pos] pos)})
+                 (nn/automate :A 100 10 419)
+                 (nn/locate 105)
+                 (nn/sample :A))
+             => (roughly 0.5))
+
+(future-fact "Complex interpolation"
+             (letfn [(interp [as bs pos]
+                       (->> (interleave as bs)
+                            (partition 2)
+                            (map (fn [[x y]] (+ x (* pos (- y x)))))))]
+               (-> (nn/initial :init {:A [0 1 0]}
+                               :interp {:A interp})
+                   (nn/automate :A 100 10 [1 0 0])
+                   (nn/locate 105)
+                   (nn/sample :A)))
+             => (roughly [0.5 0.5 0]))
