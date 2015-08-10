d04ccd4b58465d4e332796bf85f974bd793a939a
Nick (Yedikule) <nick@cassiel.com>
2015-08-10 22:29:46 +0100

    Deps.

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :signing {:gpg-key "nick@cassiel.eu"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles
   {:dev {:dependencies [[midje "1.6.3"]]
          :plugins [[lein-midje "3.1.3"]
-                   [codox "0.8.10"]]
+                   [codox "0.8.13"]]
          :codox {:output-dir "../../cassiel.gh-pages/twizzle"
                  :defaults {:doc/format :markdown}
                  :src-dir-uri "https://github.com/cassiel/twizzle/blob/master/"
